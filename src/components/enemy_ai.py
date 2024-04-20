8898ef2a984cba1bec0bfd89191ebc233376c216
Kevin Sedurifa <131756446+Paralycz@users.noreply.github.com>
2024-04-20 09:26:51 +0800

    Update enemy_ai.py

--- src/components/enemy_ai.py
+++ src/components/enemy_ai.py
@@ -4,8 +4,7 @@ def enemy_move(enemy, target):
     random_choice = choice([enemy.use_skill_1, enemy.use_skill_2])
             
     if random_choice == enemy.use_skill_2:
-        enemy.skill_2_skill_points -= 1
-        if enemy.skill_2_skill_points <= 0:
+        if enemy.skill_2_skill_points_left <= 0:
             random_choice = enemy.use_skill_1
         
     return random_choice(target)
